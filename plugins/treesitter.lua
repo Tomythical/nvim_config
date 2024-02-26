7ff9c369dc336437bd2fc6dbd216a8461a40366d
Thomas Matheickal <thomas.matheickal@fx.sgx.com>
2024-02-26 00:11:32 +0000

    LazyVim config v1.0.0

---
+++ plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "dockerfile",
+      "json",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "terraform",
+      "vim",
+      "yaml",
+    },
+  },
+}
